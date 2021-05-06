97dab784e0246de125c503472114b698ed6b0606
Jennifer Ennis <jenniferennis@jennifers-mbp.home>
2021-05-06 14:44:46 +0100

    13th commit

--- js/custom.js
+++ js/custom.js
@@ -58,7 +58,7 @@ function includeHTML() {
     /*search for elements with a certain atrribute:*/
     file = elmnt.getAttribute("w3-include-html");
     if (file) {
-      console.log(file);
+      //console.log(file);
       /* Make an HTTP request using the attribute value as the file name: */
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
