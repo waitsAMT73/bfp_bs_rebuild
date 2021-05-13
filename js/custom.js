670a2913f75e41bd18e9534f7c9a09bcaaa7597a
Jennifer Ennis <jenniferennis@jennifers-mbp.home>
2021-05-13 15:49:21 +0100

    15th commit

--- js/custom.js
+++ js/custom.js
@@ -47,16 +47,16 @@ function handleIntersect(entries, observer) {
 }
 
 
-// CODE THAT WILL INCLUDE ONE HTML PAGE INSIDE ANOTHER E.G. <div w3-include-html="content.html"></div>
+// CODE THAT WILL INCLUDE ONE HTML PAGE INSIDE ANOTHER 
 // NOTE THIS WILL ONLY WORK ON THE SERVER
-function includeHTML() {
+function incHTMLFile() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
     elmnt = z[i];
     /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
+    file = elmnt.getAttribute("custom-inc-html");
     if (file) {
       //console.log(file);
       /* Make an HTTP request using the attribute value as the file name: */
@@ -66,8 +66,8 @@ function includeHTML() {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
           /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
+          elmnt.removeAttribute("custom-inc-html");
+          incHTMLFile();
         }
       }
       xhttp.open("GET", file, true);
@@ -77,4 +77,4 @@ function includeHTML() {
     }
   }
 }
-includeHTML();
+incHTMLFile();
