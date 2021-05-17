cde23c5497f844fb8dcc72539fed16d444d51172
Jennifer Ennis <jenniferennis@jennifers-mbp.home>
2021-05-17 18:14:19 +0100

    44th commit

--- js/custom.js
+++ js/custom.js
@@ -12,8 +12,8 @@ window.addEventListener("load", (event) => {
   animatedElements = document.getElementsByClassName("animation-element");
   createObserver();
 
-  //incHTMLFile();
-  //incHTMLFilesFetch();
+  // include the header and footer //
+  incHTMLFilesFetch();
 
 }, false);
 
@@ -49,39 +49,6 @@ function handleIntersect(entries, observer) {
 
     });
 }
-
-
-// CODE THAT WILL INCLUDE ONE HTML PAGE INSIDE ANOTHER 
-// NOTE THIS WILL ONLY WORK ON THE SERVER
-function incHTMLFile() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("custominclude");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("custom-inc-html");
-    if (file) {
-
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("custom-inc-html");
-          incHTMLFile();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
-}
-incHTMLFile();
 
 function incHTMLFilesFetch() {
 
